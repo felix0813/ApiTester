89045754b6d5cfc9c10d498836c8f4b226ca68f9
felix <1306332027@qq.com>
2026-06-18 22:13:10 +0800

    feat: add Supabase configuration constants

---
+++ lib/core/constants/supabase_config.dart
@@ -0,0 +1,7 @@
+class SupabaseConfig {
+  SupabaseConfig._();
+
+  // TODO: Replace with your actual Supabase project credentials
+  static const String supabaseUrl = 'https://YOUR_PROJECT.supabase.co';
+  static const String supabaseAnonKey = 'YOUR_ANON_KEY';
+}
