cb7186928480094b8e33af8137337209f087f2d4
felix <1306332027@qq.com>
2026-06-18 20:49:47 +0800

    feat: add ShellScreen with bottom navigation

---
+++ lib/presentation/screens/mobile/shell_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../../../core/constants/app_strings.dart';
+
+class ShellScreen extends StatelessWidget {
+  final StatefulNavigationShell navigationShell;
+
+  const ShellScreen({super.key, required this.navigationShell});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: navigationShell,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: navigationShell.currentIndex,
+        onTap: (index) => navigationShell.goBranch(
+          index,
+          initialLocation: index == navigationShell.currentIndex,
+        ),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.send_outlined),
+            activeIcon: Icon(Icons.send),
+            label: AppStrings.tabRequest,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.folder_outlined),
+            activeIcon: Icon(Icons.folder),
+            label: AppStrings.tabCollections,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.layers_outlined),
+            activeIcon: Icon(Icons.layers),
+            label: AppStrings.tabEnvironments,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            activeIcon: Icon(Icons.person),
+            label: AppStrings.tabProfile,
+          ),
+        ],
+      ),
+    );
+  }
+}
